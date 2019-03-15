215957573b6c3a4fb67a8745c02794d1c28e1b86
rontend <donggyu9410@gmail.com>
2019-03-16 01:36:25 +0900

    learn basic react native - props

--- App.js
+++ App.js
@@ -1,11 +1,27 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Image } from "react-native";
+
+class Greeting extends React.Component {
+  render() {
+    return (
+      <View style={styles.GreetingContainer}>
+        <Text>Hello {this.props.name}!</Text>
+      </View>
+    );
+  }
+}
 
 export default class App extends React.Component {
   render() {
+    let pic = {
+      uri:
+        "https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg"
+    };
     return (
       <View style={styles.container}>
-        <Text>AJ</Text>
+        <Image style={styles.bananaImage} source={pic} />
+        <Greeting name={"Rontend"} />
+        <Greeting name={"AJ"} />
       </View>
     );
   }
@@ -17,5 +33,12 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center"
+  },
+  bananaImage: {
+    width: 250,
+    height: 180
+  },
+  GreetingContainer: {
+    alignItems: "center"
   }
 });
