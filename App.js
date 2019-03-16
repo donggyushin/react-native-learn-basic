5177d5e0eb657f3c6265fb3158c76e9955a65f9f
rontend <donggyu9410@gmail.com>
2019-03-16 20:35:06 +0900

    height and width, flex

--- App.js
+++ App.js
@@ -1,42 +1,13 @@
 import React from "react";
 import { StyleSheet, Text, View, Image } from "react-native";
 
-class Greeting extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isShowingText: true
-    };
-
-    setInterval(() => {
-      this.setState(previousState => ({
-        isShowingText: !previousState.isShowingText
-      }));
-    }, 1000);
-  }
-  render() {
-    if (!this.state.isShowingText) {
-      return null;
-    }
-    return (
-      <View style={styles.GreetingContainer}>
-        <Text style={styles.GreetingText}>Hello {this.props.name}!</Text>
-      </View>
-    );
-  }
-}
-
 export default class App extends React.Component {
   render() {
-    let pic = {
-      uri:
-        "https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg"
-    };
     return (
       <View style={styles.container}>
-        <Image style={styles.bananaImage} source={pic} />
-        <Greeting name={"Rontend"} />
-        <Greeting name={"AJ"} />
+        <View style={styles.powderblue} />
+        <View style={styles.skyblue} />
+        <View style={styles.steelblue} />
       </View>
     );
   }
@@ -44,19 +15,18 @@ export default class App extends React.Component {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center"
+    flex: 1
   },
-  bananaImage: {
-    width: 250,
-    height: 180
+  powderblue: {
+    backgroundColor: "powderblue",
+    flex: 1
   },
-  GreetingContainer: {
-    alignItems: "center"
+  skyblue: {
+    backgroundColor: "skyblue",
+    flex: 2
   },
-  GreetingText: {
-    color: "blue"
+  steelblue: {
+    backgroundColor: "steelblue",
+    flex: 3
   }
 });
