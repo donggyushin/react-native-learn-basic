903f9bfd5d1d17ab744b7da7d40f5558216643b9
rontend <donggyu9410@gmail.com>
2019-03-19 19:08:28 +0900

    ScrollView

--- App.js
+++ App.js
@@ -1,15 +1,5 @@
 import React from "react";
-import {
-  StyleSheet,
-  View,
-  Text,
-  Button,
-  Alert,
-  TouchableHighlight,
-  TouchableOpacity,
-  TouchableWithoutFeedback
-} from "react-native";
-import { TextInput } from "react-native-gesture-handler";
+import { StyleSheet, ScrollView, Image, Text } from "react-native";
 
 export default class App extends React.Component {
   constructor(props) {
@@ -21,92 +11,195 @@ export default class App extends React.Component {
 
   render() {
     return (
-      <View style={styles.container}>
-        <TextInput
-          style={{
-            height: 40,
-            marginTop: 40,
-            marginLeft: 40,
-            fontSize: 20
+      <ScrollView>
+        <Text style={{ fontSize: 96 }}>Scroll me plz</Text>
+        <Image
+          source={{
+            uri: "https://facebook.github.io/react-native/img/favicon.png",
+            width: 64,
+            height: 64
           }}
-          onChangeText={this.changeTextInput}
-          placeholder={"Type here to translate!"}
         />
-        <Text
-          style={{
-            padding: 10,
-            fontSize: 20
+        <Image
+          source={{
+            uri: "https://facebook.github.io/react-native/img/favicon.png",
+            width: 64,
+            height: 64
           }}
-        >
-          {this.state.text
-            .split(" ")
-            .map(word => word && "🍕")
-            .join(" ")}
-        </Text>
-        <Button
-          title={"Press Me"}
-          onPress={this.onPressButton}
-          color={"purple"}
         />
-        <TouchableHighlight onPress={this.onPressButton} underlayColor="white">
-          <View style={styles.button}>
-            <Text style={styles.buttonText}>TouchableHighlight</Text>
-          </View>
-        </TouchableHighlight>
-        <TouchableOpacity onPress={this.onPressButton}>
-          <View style={styles.button}>
-            <Text style={styles.buttonText}>TouchableOpacity</Text>
-          </View>
-        </TouchableOpacity>
-        <TouchableWithoutFeedback onPress={this.onPressButton}>
-          <View style={styles.button}>
-            <Text style={styles.buttonText}>TouchableWithoutFeedback</Text>
-          </View>
-        </TouchableWithoutFeedback>
-        <TouchableHighlight
-          underlayColor={"white"}
-          onPress={this.onPressButton}
-          onLongPress={this.onLongPressButton}
-        >
-          <View style={styles.button}>
-            <Text style={styles.buttonText}>
-              TouchableHighlight with Long Press
-            </Text>
-          </View>
-        </TouchableHighlight>
-      </View>
+        <Image
+          source={{
+            uri: "https://facebook.github.io/react-native/img/favicon.png",
+            width: 64,
+            height: 64
+          }}
+        />
+        <Image
+          source={{
+            uri: "https://facebook.github.io/react-native/img/favicon.png",
+            width: 64,
+            height: 64
+          }}
+        />
+        <Image
+          source={{
+            uri: "https://facebook.github.io/react-native/img/favicon.png",
+            width: 64,
+            height: 64
+          }}
+        />
+        <Text style={{ fontSize: 96 }}>If you like</Text>
+        <Image
+          source={{
+            uri: "https://facebook.github.io/react-native/img/favicon.png",
+            width: 64,
+            height: 64
+          }}
+        />
+        <Image
+          source={{
+            uri: "https://facebook.github.io/react-native/img/favicon.png",
+            width: 64,
+            height: 64
+          }}
+        />
+        <Image
+          source={{
+            uri: "https://facebook.github.io/react-native/img/favicon.png",
+            width: 64,
+            height: 64
+          }}
+        />
+        <Image
+          source={{
+            uri: "https://facebook.github.io/react-native/img/favicon.png",
+            width: 64,
+            height: 64
+          }}
+        />
+        <Image
+          source={{
+            uri: "https://facebook.github.io/react-native/img/favicon.png",
+            width: 64,
+            height: 64
+          }}
+        />
+        <Text style={{ fontSize: 96 }}>Scrolling down</Text>
+        <Image
+          source={{
+            uri: "https://facebook.github.io/react-native/img/favicon.png",
+            width: 64,
+            height: 64
+          }}
+        />
+        <Image
+          source={{
+            uri: "https://facebook.github.io/react-native/img/favicon.png",
+            width: 64,
+            height: 64
+          }}
+        />
+        <Image
+          source={{
+            uri: "https://facebook.github.io/react-native/img/favicon.png",
+            width: 64,
+            height: 64
+          }}
+        />
+        <Image
+          source={{
+            uri: "https://facebook.github.io/react-native/img/favicon.png",
+            width: 64,
+            height: 64
+          }}
+        />
+        <Image
+          source={{
+            uri: "https://facebook.github.io/react-native/img/favicon.png",
+            width: 64,
+            height: 64
+          }}
+        />
+        <Text style={{ fontSize: 96 }}>What's the best</Text>
+        <Image
+          source={{
+            uri: "https://facebook.github.io/react-native/img/favicon.png",
+            width: 64,
+            height: 64
+          }}
+        />
+        <Image
+          source={{
+            uri: "https://facebook.github.io/react-native/img/favicon.png",
+            width: 64,
+            height: 64
+          }}
+        />
+        <Image
+          source={{
+            uri: "https://facebook.github.io/react-native/img/favicon.png",
+            width: 64,
+            height: 64
+          }}
+        />
+        <Image
+          source={{
+            uri: "https://facebook.github.io/react-native/img/favicon.png",
+            width: 64,
+            height: 64
+          }}
+        />
+        <Image
+          source={{
+            uri: "https://facebook.github.io/react-native/img/favicon.png",
+            width: 64,
+            height: 64
+          }}
+        />
+        <Text style={{ fontSize: 96 }}>Framework around?</Text>
+        <Image
+          source={{
+            uri: "https://facebook.github.io/react-native/img/favicon.png",
+            width: 64,
+            height: 64
+          }}
+        />
+        <Image
+          source={{
+            uri: "https://facebook.github.io/react-native/img/favicon.png",
+            width: 64,
+            height: 64
+          }}
+        />
+        <Image
+          source={{
+            uri: "https://facebook.github.io/react-native/img/favicon.png",
+            width: 64,
+            height: 64
+          }}
+        />
+        <Image
+          source={{
+            uri: "https://facebook.github.io/react-native/img/favicon.png",
+            width: 64,
+            height: 64
+          }}
+        />
+        <Image
+          source={{
+            uri: "https://facebook.github.io/react-native/img/favicon.png",
+            width: 64,
+            height: 64
+          }}
+        />
+        <Text style={{ fontSize: 80 }}>React Native</Text>
+      </ScrollView>
     );
   }
-
-  onPressButton = () => {
-    Alert.alert("You tapped the button!");
-  };
-
-  onLongPressButton = () => {
-    Alert.alert("You long-pressed the button!");
-  };
-
-  changeTextInput = text => {
-    this.setState({
-      text
-    });
-  };
 }
 
 const styles = StyleSheet.create({
   container: {
-    padding: 60,
-    alignItems: "center"
-  },
-  button: {
-    width: 260,
-    marginBottom: 30,
-    alignItems: "center",
-    backgroundColor: "#2196F3",
-    borderRadius: 15
-  },
-  buttonText: {
-    padding: 20,
-    color: "white"
+    padding: 60
   }
 });
