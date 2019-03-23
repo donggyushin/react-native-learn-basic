479ca89881514afe02e42d40c2a22c7bb1178718
rontend <donggyu9410@gmail.com>
2019-03-23 17:29:40 +0900

    I did learn something...

--- App.js
+++ App.js
@@ -1,28 +1,39 @@
 import React from "react";
-import { View, Button } from "react-native";
+import { MaskedViewIOS, Text, View } from "react-native";
 
 export default class App extends React.Component {
   render() {
-    const { clickButton } = this;
     return (
-      <View
-        style={{
-          flex: 1,
-          alignItems: "center",
-          justifyContent: "center"
-        }}
+      // Determines shape of the mask
+      <MaskedViewIOS
+        style={{ flex: 1, flexDirection: "row", height: "100%" }}
+        maskElement={
+          <View
+            style={{
+              // Transparent background because mask is based off alpha channel.
+              backgroundColor: "transparent",
+              flex: 1,
+              justifyContent: "center",
+              alignItems: "center"
+            }}
+          >
+            <Text
+              style={{
+                fontSize: 60,
+                color: "black",
+                fontWeight: "bold"
+              }}
+            >
+              Basic Mask
+            </Text>
+          </View>
+        }
       >
-        <Button
-          onPress={clickButton}
-          title={"Learn More"}
-          color={"#841584"}
-          accessibilityLabel="Learn more about this purple button"
-        />
-      </View>
+        {/* Shows behind the mask, you can put anything here, such as an image */}
+        <View style={{ flex: 1, height: "100%", backgroundColor: "#324376" }} />
+        <View style={{ flex: 1, height: "100%", backgroundColor: "#F5DD90" }} />
+        <View style={{ flex: 1, height: "100%", backgroundColor: "#F76C5E" }} />
+      </MaskedViewIOS>
     );
   }
-
-  clickButton = () => {
-    console.log("you pressed button");
-  };
 }
