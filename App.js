224fdd775c05739d031b851d021da94097395940
rontend <donggyu9410@gmail.com>
2019-03-23 15:10:29 +0900

    loading component

--- App.js
+++ App.js
@@ -1,52 +1,27 @@
 import React from "react";
-import { ActivityIndicator, Text, View } from "react-native";
-import { FlatList } from "react-native-gesture-handler";
+import { ActivityIndicator, StyleSheet, View } from "react-native";
 
 export default class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isLoading: true
-    };
-  }
-
-  componentDidMount() {
-    return fetch(`https://facebook.github.io/react-native/movies.json`)
-      .then(response => response.json())
-      .then(responseJson => {
-        console.log(responseJson);
-        this.setState(
-          {
-            isLoading: false,
-            dataSource: responseJson.movies
-          },
-          () => {}
-        );
-      })
-      .catch(error => {
-        console.log(error);
-      });
-  }
   render() {
-    if (this.state.isLoading) {
-      return (
-        <View style={{ flex: 1, paddingTop: 20 }}>
-          <ActivityIndicator />
-        </View>
-      );
-    }
     return (
-      <View style={{ flex: 1, paddingTop: 20 }}>
-        <FlatList
-          data={this.state.dataSource}
-          keyExtractor={({ id }, index) => id}
-          renderItem={({ item }) => (
-            <Text>
-              {item.title}, {item.releaseYear}
-            </Text>
-          )}
-        />
+      <View style={[styles.container, styles.horizontal]}>
+        <ActivityIndicator size="large" color="#0000ff" />
+        <ActivityIndicator size="small" color="#00ff00" />
+        <ActivityIndicator size="large" color="#0000ff" />
+        <ActivityIndicator size="small" color="#00ff00" />
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center"
+  },
+  horizontal: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    padding: 10
+  }
+});
